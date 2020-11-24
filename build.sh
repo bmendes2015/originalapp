abe320a35ab2795e2d7574f84698cc4a9ef16982
Bruno Queiroz Mendes <bruno.com.br@gmail.com>
2020-11-24 14:22:38 -0300

    commit final

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -euxo pipefail
 
 docker build . -t originalapp
+docker run -d -p 8080:8080 -v "$HOME/.m2":/root/.m2 --rm originalapp
 
-
-docker run -p 8080:8080 -v "$HOME/.m2":/root/.m2 --rm originalapp
+#frontend
+./frontend/build.sh
--- src/main/java/br/com/orginalapp/crud/controller/PessoaController.java
+++ src/main/java/br/com/orginalapp/crud/controller/PessoaController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import br.com.orginalapp.crud.exception.RecursoNaoEncontradoException;
 import br.com.orginalapp.crud.service.PessoaService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
 public class PessoaController {
 	
